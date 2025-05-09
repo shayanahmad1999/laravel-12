3c2b4da45b1bd748f889ed8a628febd0d6af6d7c
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-09 16:21:30 +0500

    Eloquent Relations and GET Parameters

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -10,6 +11,9 @@ class BlogController extends Controller
     public function index()
     {
         $categories = Category::all();
+        $posts = Post::when(request('category_id'), function ($query) {
+            $query->where('category_id', request('category_id'));
+        })->latest()->get();
         return view('blog', get_defined_vars());
     }
 }

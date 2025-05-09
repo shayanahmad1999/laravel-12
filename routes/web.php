2d4293e88535b225958ef8e89c3d7fdcaa0c3c28
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-09 16:15:01 +0500

    MVC, DB Queries, and Eloquent Models

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/welcome', function () {
@@ -14,7 +15,7 @@ Route::get('/second', function () {
 Route::view('/second', 'second');
 
 Route::view('/', 'home')->name('home');
-Route::view('blog', 'blog')->name('blog');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 

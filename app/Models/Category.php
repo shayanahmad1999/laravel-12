e8d388073ad4c4690d26258f0e137f5dfff0a0e0
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-09 18:40:43 +0500

    define relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $table = 'categories';
     protected $fillable = ['name'];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }

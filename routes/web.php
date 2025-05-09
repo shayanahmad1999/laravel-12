e14b1fea4d33a4d376de9a07e918963cc1104120
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-09 17:46:26 +0500

    Admin User, Route Groups, Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,25 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::resource('categories', CategoryController::class)
+        ->middleware(IsAdminMiddleware::class);
 });
 
 Route::view('/second', 'second');
@@ -27,7 +32,5 @@ Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 
 Route::view('article', 'article')->name('article');
-
-Route::resource('categories', CategoryController::class);
 
 require __DIR__ . '/auth.php';
